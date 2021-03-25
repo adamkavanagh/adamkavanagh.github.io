9c01382b029f7574d5b4a807988193846ed3cf49
Adam Kavanagh <adam.kavanagh@sap.com>
2021-03-25 16:17:41 +0000

    Start page at top

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,15 +1,16 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
 ], function (Controller, MessageToast) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.wt.controller.HelloPanel", {
-	   contactDetails : function () {
-  // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sMsg = oBundle.getText("contact");
-         // show message
-         MessageToast.show(sMsg);
-	   }
-   });
+	"use strict";
+	return Controller.extend("sap.ui.demo.wt.controller.HelloPanel", {
+		contactDetails: function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sMsg = oBundle.getText("contact");
+			// show message
+			MessageToast.show(sMsg);
+		}
+
+	});
 });
---
+++ controller/Overview.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.wt.controller.Overview", {
+
+		onInit: function () {
+			var route = this.getOwnerComponent().getRouter().getRoute("overview");
+			route.attachPatternMatched(this.onPagePatternMatched, this);
+		},
+
+		onPagePatternMatched: function () {
+			this.byId("overview").scrollTo(0);
+		}
+
+	});
+});
